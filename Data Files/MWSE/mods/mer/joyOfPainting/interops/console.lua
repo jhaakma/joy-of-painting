4625f7fa9923cce2b0b3ce4a200d5d74551c6fc2
jhaakma <jchaakma@gmail.com>
2023-06-10 13:52:04 +1200

    Remove coc command

--- Data Files/MWSE/mods/mer/joyOfPainting/interops/console.lua
+++ Data Files/MWSE/mods/mer/joyOfPainting/interops/console.lua
@@ -3,14 +3,5 @@ event.register("UIEXP:sandboxConsole", function(e)
     e.sandbox.jop = {
         skills = SkillService.skills
     }
-    e.sandbox.coc = function(cellName)
-        local cell = tes3.getCell({ id = cellName })
-        if cell.isInterior then
-            tes3.positionCell({
-                cell = cell,
-                position = {x = 0, y = 0, z = 0},
-                reference = tes3.player
-            })
-        end
-    end
+
 end)
